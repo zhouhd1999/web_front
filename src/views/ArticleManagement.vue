88b34d3dc317861217448120db4d5e90eb2595d1
DELL <719887797@qq.com>
2019-12-19 02:49:27 +0800

    完成

--- src/views/ArticleManagement.vue
+++ src/views/ArticleManagement.vue
@@ -2,6 +2,7 @@
     <div>
         <el-button @click="showArtileType()" autofocus="true">生活笔记</el-button>
         <el-button @click="showArtileType_1()">技术杂谈</el-button>
+        <el-button @click="showArtileType_2()">未归档</el-button>
         <div style="margin-top: -130px; height: 200px;">
             <el-collapse-transition>
                 <div>
@@ -38,14 +39,26 @@
                             <div style="margin: 15px;float: right;">
                                 <el-button type="primary" @click="isEdit(item,index)">{{editShow}}</el-button>
                                 <el-button type="danger" @click="delArticle(item,index)">删除</el-button>
-
-
                             </div>
                         </div>
                     </div>
                 </div>
             </el-collapse-transition>
         </div>
+        <el-dialog title="选择标签" :visible.sync="dialogFormVisible">
+            <el-select v-model="value" placeholder="请选择">
+                <el-option
+                        v-for="item in form"
+                        :key="item.tagId"
+                        :label="item.tagName"
+                        :value="item.tagId">
+                </el-option>
+            </el-select>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="closeModel">取 消</el-button>
+                <el-button type="primary" @click="handTagId()">确 定</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -57,22 +70,44 @@
         editShow:"编辑",
         article: [],
         articles:[],
+        chooseTagId:'',
+        curTagId:'',
+        dialogTableVisible: false,
+        dialogFormVisible: false,
+        form: [{
+            tagId:'',
+            tagName:''
+        }],
+            value: ''
     }
     },
         methods:{
 
+            closeModel(){
+                this.dialogFormVisible = false;
+            },
+            handTagId(){
+               console.log(this.value)
+                this.chooseTagId = this.value;
+                this.editShow = "完成";
+                this.dialogFormVisible = false;
+            },
             isEdit(item,index){
+                this.curTagId = item.tagId;
+                this.get_tag(this.curTagId);
                 if(this.editShow == "编辑"){
-                    this.editShow = "完成";
+                    this.dialogFormVisible = true;
+
                     console.log(this.article[index].isEdits)
                     this.article[index].isEdits = false;
                 }
                 else if(this.editShow =="完成"){
                     this.editShow = "编辑";
                     this.article[index].isEdits = true;
-                    console.log(item)
+                    this.articles[index].article.tagId= this.chooseTagId;
                     this.articles[index].article.articleContent = item.articleContent;
                     var data = this.articles[index].article;
+                    console.log(data)
                     this.$req.post('/article/update_article',data)
                     .then(res => {
                         if (res.code === 0) {
@@ -96,26 +131,48 @@
                // this.show3=!this.show3
                 this.get_article(1);
             },
-
+            showArtileType_2(){
+                this.get_article(2);
+            },
 
             ShowArticle(index){
                 this.article[index].isShow=!this.article[index].isShow;
                 console.log(this.article[index].isShow)
 
             },
+             get_tag(tagid){
+                var befortagId=tagid;
+                console.log("原来文章的tagId"+befortagId)
+                 this.form=[];
+                this.$req.post('/tag/get_all_tag')
+                  .then(res=>{
+                      if (res.code===0){
+                          for (var i=0;i<res.data.length;i++){
+                              this.form.push({
+                                  tagId:'',
+                                  tagName:''
+                              });
+                              this.form[i].tagName=res.data[i].tagName;
+                              this.form[i].tagId=res.data[i].tagId;
+
+                          }
+                      }
+                  })
+            },
             get_article:function (sta) {
                 this.article=[];
                 this.articles = [];
-                var data={
-                    state:sta
-                }
-                this.$req.post('/article/get_article_by_state',data)
+                console.log(sta)
+                this.$req.post('/article/get_article_by_tag_type',{
+                    tagType:sta
+                })
                     .then(res => {
                         if (res.code === 0) {
                             this.articles = res.data;
                             console.log(res.data)
                             for (var i=0;i<res.data.length;i++){
                                 this.article.push({
+                                    tagId:'',
                                     articleName:'',
                                     nickname:'',
                                     articleContent:'',
@@ -123,14 +180,17 @@
                                     isEdits:true,
                                 });
                             }
+
                             for(let i = 0; i < res.data.length;i++)
                             {
+                                this.article[i].tagId = res.data[i].article.articleId;
                                 this.article[i].articleName=res.data[i].article.articleName;
                                 this.article[i].articleDescribe=res.data[i].article.articleDescribe;
                                 this.article[i].nickname=res.data[i].nickname;
                                 this.article[i].articleContent=res.data[i].article.articleContent;
                             }
-
+                            this.$message.success("成功！")
+                            console.log(this.article)
                         } else {
                             this.$message({
                                 type: 'error',
--- src/views/TagManagement.vue
+++ src/views/TagManagement.vue
@@ -1,33 +1,13 @@
 <template>
     <div style="min-height: 1000px">
-        <div>
-            <el-tag
-                    :key="tag.tagName"
-                    v-for="tag in this.tags1"
-                    closable
-                    :disable-transitions="false"
-                    @close="handleClose1(tag)">
-                {{tag.tagName}}
-            </el-tag>
-            <el-input
-                    class="input-new-tag"
-                    v-if="inputVisible1"
-                    v-model="inputValue1"
-                    ref="saveTagInput"
-                    size="small"
-                    @keyup.enter.native="handleInputConfirm1"
-                    @blur="handleInputConfirm1"
-            >
-            </el-input>
-            <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
-        </div>
-        <div>
+        <div style="width: 1000px;height: 100px">
             <el-tag
                     :key="tag.tagName"
                     v-for="tag in this.tags0"
                     closable
                     :disable-transitions="false"
-                    @close="handleClose0(tag)">
+                    @close="handleClose0(tag)"
+                    style="margin-right: 50px">
                 {{tag.tagName}}
             </el-tag>
             <el-input
@@ -40,7 +20,32 @@
                     @blur="handleInputConfirm0"
             >
             </el-input>
-            <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+            <el-button v-else class="button-new-tag" size="small" style="margin-right:50px;margin-top: 25px" @click="showInput0">+ 新增生活笔记</el-button>
+        </div>
+        <el-divider></el-divider>
+
+        <div style="width: 1000px;height: 50px;">
+            <el-tag
+                    :key="tag.tagName"
+                    v-for="tag in this.tags1"
+                    closable
+                    :disable-transitions="false"
+                    @close="handleClose1(tag)"
+                    style="margin-right: 50px">
+                {{tag.tagName}}
+            </el-tag>
+            <el-input
+
+                    class="input-new-tag"
+                    v-if="inputVisible1"
+                    v-model="inputValue1"
+                    ref="saveTagInput"
+                    size="small"
+                    @keyup.enter.native="handleInputConfirm1"
+                    @blur="handleInputConfirm1"
+            >
+            </el-input>
+            <el-button v-else class="button-new-tag" size="small"  style="margin-right:50px;margin-top: 25px" @click="showInput1">+ 新增技术杂谈</el-button>
         </div>
 
     </div>
@@ -51,8 +56,8 @@
         name: "TagManagement",
         data(){
            return{
-               tags1:[],
                tags0:[],
+               tags1:[],
                inputVisible1: false,
                inputValue1: '',
                inputVisible0: false,
@@ -60,37 +65,49 @@
            }
         },
         methods: {
-            handleClose1(tag) {
-                console.log(tag.tagId)
-                var data={
-                    tagId:tag.tagId
-                };
-                this.$req.post('/tag/delete_tag',data)
-                    .then(res=>{
-                        if (res.code===0)
-                        {
-                            this.tags1.splice(this.tags1.indexOf(tag), 1);
-                            this.getAllTag()
-                            this.$message.success("删除标签成功");
-                        }
-                        else {
-                            this.$message.error("删除标签失败");
-                        }
-                    })
-
-            },
+            // handleClose1(tag) {
+            //     var data={
+            //         tagId:tag.tagId
+            //     };
+            //     console.log(data)
+            //     this.$req.post('/tag/delete_tag',data)
+            //         .then(res=>{
+            //             if (res.code===0)
+            //             {
+            //                 this.tags1.splice(this.tags1.indexOf(tag), 1);
+            //                 this.getAllTag();
+            //                 this.$message.success("删除标签成功");
+            //             }
+            //             else if(res.code===601){
+            //                 this.$message.error("标签内有文章");
+            //                 return;
+            //             }
+            //             else {
+            //                 this.$message.error("删除标签失败");
+            //             }
+            //         })
+            //
+            // },
             handleClose0(tag) {
-                console.log(tag.tagId);
                 var data={
                     tagId:tag.tagId
                 };
+                console.log(tag)
+                // console.log(tag.tagId);
                 this.$req.post('/tag/delete_tag',data)
                     .then(res=>{
                         if (res.code===0)
                         {
-                            this.tags0.splice(this.tags0.indexOf(tag), 1);
-                            this.getAllTag()
+                            console.log(tag.tagType)
+                            if(tag.tagType == 0) {
+                                this.tags0.splice(this.tags0.indexOf(tag), 1);
+                            }
+
+                            this.getAllTag();
                             this.$message.success("删除标签成功");
+                        }else if(res.code===601){
+                            this.$message.error("标签内有文章");
+                            return;
                         }
                         else {
                             this.$message.error("删除标签失败");
@@ -98,25 +115,49 @@
                     })
 
             },
-            showInput() {
-                this.inputVisible1 = true;
-                this.$nextTick(_ => {
-                    this.$refs.saveTagInput.$refs.input.focus();
-                });
+
+            handleClose1(tag) {
+                var data={
+                    tagId:tag.tagId
+                };
+                console.log(tag)
+                // console.log(tag.tagId);
+                this.$req.post('/tag/delete_tag',data)
+                    .then(res=>{
+                        if (res.code===0)
+                        {
+                            console.log(tag.tagType)
+
+                            if(tag.tagType == 1){
+                                this.tags1.splice(this.tags1.indexOf(tag), 1);
+                            }
+                            this.getAllTag();
+                            this.$message.success("删除标签成功");
+                        }else if(res.code===601){
+                            this.$message.error("标签内有文章");
+                            return;
+                        }
+                        else {
+                            this.$message.error("删除标签失败");
+                        }
+                    })
+
             },
             handleInputConfirm0() {
                 let inputValue0 = this.inputValue0;
                 var data = {
                     tagName: inputValue0,
                     tagType: 0
-                }
+                };
                 if (inputValue0) {
                     this.$req.post('/tag/insert_tag', data)
                         .then(res => {
                             if (res.code === 0) {
                                 this.tags1.push({
                                     tagName: inputValue0,
+                                    tagType: 0
                                 });
+                                this.getAllTag();
                                 this.$message.success("标签添加成功！")
                             } else {
                                 this.$message.error("标签添加失败！")
@@ -125,20 +166,22 @@
                 }
                 this.inputVisible0 = false;
                 this.inputValue0 = '';
-
             },
             handleInputConfirm1() {
                 let inputValue1 = this.inputValue1;
                 var data={
                     tagName:inputValue1,
+                    tagType: 1
                 };
                 if (inputValue1) {
                     this.$req.post('/tag/insert_tag',data)
                         .then(res=>{
                             if (res.code===0){
                                 this.tags1.push({
-                                    tagName:inputValue1
+                                    tagName:inputValue1,
+                                    tagType:1
                                 });
+                                this.getAllTag();
                                 this.$message.success("标签添加成功！")
                             }
                             else {
@@ -157,12 +200,13 @@
                     .then(res => {
                         if (res.code === 0) {
                             for(var i = 0; i < res.data.length; i++){
-                                if(res.data[i].tagType == 1){
+                                if(res.data[i].tagType === 1){
                                     this.tags1.push(res.data[i]);
                                 }
-                                else if(res.data[i].tagType == 0){
+                                else if(res.data[i].tagType === 0){
                                     this.tags0.push(res.data[i]);
                                 }
+
                             }
 
                         } else {
@@ -174,8 +218,13 @@
                     })
             },
 
-
-            showInput() {
+            showInput1() {
+                this.inputVisible1 = true;
+                this.$nextTick(_ => {
+                    this.$refs.saveTagInput.$refs.input.focus();
+                });
+            },
+            showInput0() {
                 this.inputVisible0 = true;
                 this.$nextTick(_ => {
                     this.$refs.saveTagInput.$refs.input.focus();
@@ -211,5 +260,8 @@
         width: 90px;
         margin-left: 10px;
         vertical-align: bottom;
+    }
+    .tagg{
+
     }
 </style>
